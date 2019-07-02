5b89162329ee4ca8c6f1a1a5d04c6f0e17f9ee18
tomfraczek <tomaszfr90@gmail.com>
2019-07-02 13:01:24 +0100

    build 2.0

---
+++ build/precache-manifest.b3941f26511c1d6cdbfa1ba97ccdf299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc62da302ebe69115b56cf616f68602b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb78f38977ebc0432f41",
+    "url": "/static/css/main.46d1479c.chunk.css"
+  },
+  {
+    "revision": "062d3d3b8b75c5b7878a",
+    "url": "/static/js/2.0eb29630.chunk.js"
+  },
+  {
+    "revision": "eb78f38977ebc0432f41",
+    "url": "/static/js/main.10491c99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "09cfe256212b76eed2c5e1cbde567ed0",
+    "url": "/static/media/spinner.09cfe256.svg"
+  }
+]);
